5a4d54e00e1c6adf28b5afbd38126181f57443c9
levkopo <40952805+levkopo@users.noreply.github.com>
2021-03-22 16:32:29 +0500

    Initial commit

---
+++ src/main/java/com/github/kayjamlang/request/RequestLibrary.java
@@ -0,0 +1,60 @@
+package com.github.kayjamlang.request;
+
+import com.github.kayjamlang.core.Argument;
+import com.github.kayjamlang.core.Type;
+import com.github.kayjamlang.core.containers.ClassContainer;
+import com.github.kayjamlang.executor.libs.Library;
+import com.github.kayjamlang.executor.libs.main.MapClass;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Map;
+
+public class RequestLibrary extends Library {
+
+    public RequestLibrary() throws Exception {
+        addFunction(new LibFunction("buildHttpQuery", (mainContext, context) -> {
+            Map<Object, Object> queryMap = ((MapClass) context.variables.get("query"))
+                    .getMap(context);
+
+            StringBuilder query = new StringBuilder();
+            for(Map.Entry<Object, Object> entry: queryMap.entrySet()){
+                query.append(query.length()==0?"?":"&")
+                        .append(entry.getKey().toString())
+                        .append(URLEncoder.encode(entry.getValue().toString(),
+                                "UTF-8"));
+            }
+
+            return query.toString();
+        }, new Argument(new Type("map", ClassContainer.class, false),
+                "query")));
+
+        classes.put("request", new LibClass("request", (clazz)->{
+            clazz.addVariable("method", "GET");
+            clazz.addConstructor(new LibConstructor((mainContext, context) -> {
+                    context.parentContext.variables.put("url", context.variables.get("url"));
+                    return null;
+            }, new Argument(Type.STRING, "url")));
+
+            clazz.addFunction(new LibFunction("execute", (mainContext, context) -> {
+                URL obj = new URL((String) context.parentContext.variables.get("url"));
+                HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+
+                connection.setRequestMethod((String) context.parentContext.variables.get("method"));
+
+                StringBuilder response = new StringBuilder();
+                try(BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))){
+                    String inputLine;
+
+                    while ((inputLine = in.readLine()) != null)
+                        response.append(inputLine);
+                }
+
+                return response.toString();
+            }));
+        }));
+    }
+}
